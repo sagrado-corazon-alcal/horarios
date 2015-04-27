05120a4af2a11fcb632197616b62fe2dfbafc60e
Federico Aloi <federico.aloi@gmail.com>
2015-04-27 18:23:41 -0300

    Funcionando horario por profe, sin desdoble

--- server/api/horario/horario.controller.js
+++ server/api/horario/horario.controller.js
@@ -1,7 +1,41 @@
 'use strict';
 
+var _ = require('lodash');
+
 var horarios = require('./horario.data');
 
+var getHorarios = function (profesor, dia) {
+  return _(horarios)
+  .map(function(horario) {
+    return {
+      division: horario.division,
+      horarios: _.pick(horario.horarios[dia], function (horariosDelDia) {
+        return _.any(horariosDelDia, { profesor: profesor });
+      })
+    };
+  })
+  .reject(function(it) { return _.isEmpty(it.horarios); })
+  .map(function(horario) {
+    return _.mapValues(horario.horarios, function (it) {
+      return {
+        nombre: it[0].nombre,
+        division: horario.division
+      };
+    });
+  })
+  .flatten()
+  .foldl(_.merge);
+
+  //TODO: no funca bien con los grupos
+};
+
 exports.index = function(req, res) {
-  return res.json(200, horarios);
+  if (req.query.profesor == null)
+    return res.json(200, horarios);
+  else {
+    var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+    var horarioPorProfe = _.zipObject(dias, _.map(dias, _.partial(getHorarios, req.query.profesor)));
+
+    return res.json(200, horarioPorProfe);
+  }
 };
--- server/api/horario/horario.spec.js
+++ server/api/horario/horario.spec.js
@@ -17,4 +17,35 @@ describe('GET /api/horarios', function() {
         done();
       });
   });
+
+  it('cuando se le pasa un profesor solo devuelve los horarios de ese', function(done) {
+    request(app)
+      .get('/api/horarios?profesor=Páramo')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.eql({
+          lunes: {
+            5: { nombre: "Ciencias Sociales", division: "1°A" }
+          },
+          martes: {
+            5: { nombre: "Ciencias Sociales", division: "1°A" },
+            6: { nombre: "Construcción de la Ciudadanía", division: "2°B" },
+            7: { nombre: "Construcción de la Ciudadanía", division: "2°B" },
+            8: { nombre: "Historia", division: "3°B" },
+            9: { nombre: "Historia", division: "3°B" }
+          },
+          miercoles: {
+            5: { nombre: "Ciencias Sociales", division: "1°A" }
+          },
+          viernes: {
+            5: { nombre: "Ciencias Sociales", division: "1°A" },
+            6: { nombre: "Construcción de la Ciudadanía", division: "1°B" },
+            7: { nombre: "Construcción de la Ciudadanía", division: "1°B" }
+          }
+        });
+        done();
+      });
+  });
 });
