ac73c2db2089012555978099717e3e57b5c0aa5c
Federico Aloi <federico.aloi@gmail.com>
2015-04-28 01:30:08 -0300

    La busqueda de horario por profe es case insensitive

--- server/api/horario/horario.controller.js
+++ server/api/horario/horario.controller.js
@@ -4,20 +4,24 @@ var _ = require('lodash');
 
 var horarios = require('./horario.data');
 
+var isProfesorLike = function (expected, horario) {
+  return new RegExp("^" + expected + "$", "i").test(horario.profesor)
+};
+
 var getHorarios = function (profesor, dia) {
   return _(horarios)
   .map(function(horario) {
     return {
       division: horario.division,
       horarios: _.pick(horario.horarios[dia], function (horariosDelDia) {
-        return _.any(horariosDelDia, { profesor: profesor });
+        return _.any(horariosDelDia, _.partial(isProfesorLike, profesor));
       })
     };
   })
   .reject(function(it) { return _.isEmpty(it.horarios); })
   .map(function(horario) {
     return _.mapValues(horario.horarios, function (it) {
-      var hora = _.find(it, { profesor: profesor });
+      var hora = _.find(it, _.partial(isProfesorLike, profesor));
 
       return {
         nombre: hora.nombre,
@@ -28,14 +32,12 @@ var getHorarios = function (profesor, dia) {
   })
   .flatten()
   .foldl(_.merge);
-
-  //TODO: no funca bien con los grupos
 };
 
 exports.index = function(req, res) {
-  if (req.query.profesor == null)
+  if (req.query.profesor == null) {
     return res.json(200, horarios);
-  else {
+  } else {
     var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
     var horarioPorProfe = _.zipObject(dias, _.map(dias, _.partial(getHorarios, req.query.profesor)));
 
